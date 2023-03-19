13c14338e260cb338e00bba64436d329fb23e544
Julián Lapunzina <102098759+JulianLapunzina@users.noreply.github.com>
2023-03-18 21:38:31 -0300

    Desafío 1 completo, mejorado y terminado.

--- Desafio-1.js
+++ Desafio-1.js
@@ -1,49 +1,93 @@
+        // Desafío N° 1 - Julián Lapunzina
+
+const products = []
+
 class ProductManager {
-
     constructor() {
-        this.products = []
+        this.products = products
     }
 
-    addProduct(title,description,price,thumbnail,code,stock) {
-    const product ={
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock
+    addProduct(product){
+    //valida que todos los campos estén completos
+    if(!product.title ||
+    !product.description ||
+    !product.price ||
+    !product.thumbnail ||
+    !product.code ||
+    !product.stock) return console.log("Every fields are request")
+    
+    // valida el código del producto, si ya existe lo reporta por consola
+    let productPushed = this.products.find(prod => prod.code === product.code)
+    if(productPushed) return console.log(`This products was already pushed, here's the code:"${product.code}"`)
+
+    //le asigna un id al producto agregado
+    return this.products.push({id: this.products.length+1, ...product})
     }
-    if(this.products.length === 0){
-        product.id = 1
-    } else {
-        product.id = this.products[this.products.length - 1].id + 1
-    }
-    if (Object.values(product).every(value=>value)){
-        this.products.push(product);
-    } else {
-        console.log("Necesita rellenar todos los campos")
-    }
-}
-    getProducts() {
+
+    getProducts(){
         return this.products
+    }
+
+    getProductById(id){
+        let product = this.products.find(prod => prod.id === id)
+        if(!product) return 'Product not Found'
+        return product
         }
-
-    getProductsById(id) {
-        const obj = this.products.find(product=> product.id === id)
-        return obj ? obj : console.log("No products find")
-    }
     }
 
-const app = new ProductManager()
+const product = new ProductManager()
+
+product.addProduct({
+    title: "El oficio de enamorarse",
+    description: "Poesia",
+    price: 200,
+    thumbnail: "public/images/ElOficio.jpg",
+    code: "ElOf123",
+    stock: 5
+})
+
+product.addProduct({
+    title: "Tan cerca, Tan cerca",
+    description: "Cuentos",
+    price: 300,
+    thumbnail: "public/images/tanC.png",
+    code: "TanC123",
+    stock: 4
+}
+)
+
+// Segundo producto igual para comprobar la validación del código
+
+product.addProduct({
+    title: "Tan cerca, Tan cerca",
+    description: "Cuentos",
+    price: 300,
+    thumbnail: "public/images/tanC.png",
+    code: "TanC123",
+    stock: 4
+}
+
+)
+product.addProduct({
+    title: "El señor de los anillos",
+    description: "Novela",
+    price: 1500,
+    thumbnail: "public/images/LOR.jpg",
+    code: "Lor123",
+    stock: 8
+})
+
+//Acá dejo un producto con un campo sin rellenar para checkear el return de los campos incompletos
+
+product.addProduct({
+    description: "Novela",
+    price: 1500,
+    thumbnail: "public/images/LOR.jpg",
+    code: "Lor123",
+    stock: 8
+})
 
 
-app.addProduct("El oficio de enamorarse", "libro de poesía", 200, "../img/ElOficio.jpg","ElOf123", 5)
-app.addProduct("Tan cerca, Tan cerca", "libro de cuentos", 300, "../img/TanCerca.jpg","TanC123", 4)
-app.addProduct("El señor de los anillos", "Novela", 1500, "../img/LOR.jpg","Lor123", 8)
-
-
-
-console.log("Productos en stock",app.getProducts())
-console.log("Producto con id 3", app.getProductsById(3))
-
+console.log("Products stock: \n",product.getProducts())
+console.log("Products with id: \n", product.getProductById(2))
 
